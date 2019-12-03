a810f7e8aea58abdd555202dec2e02d5dfe541c4
Stackingrule <messageswitching@gmail.com>
2019-12-03 23:01:39 +0800

    start Java-Structure

---
+++ Data_Structure/Java-Data-Structure/src/Bag/ArrayBag.java
@@ -0,0 +1,6 @@
+package Bag;
+import Bag.BagInterface;
+public class ArrayBag<T> implements BagInterface<T> {
+
+
+}
---
+++ Data_Structure/Java-Data-Structure/src/Bag/BagInterface.java
@@ -0,0 +1,24 @@
+package Bag;
+import Bag.ArrayBag;
+
+public class BagInterface<T>
+{
+    public int getCurrentSize();
+
+    public boolean isEmpty();
+
+    public boolean add(T newEntry);
+
+    public T remove();
+
+    public boolean remove(T anEntry);
+
+    public void clear();
+
+    public int getFrequencyOf(T anEntry);
+
+    public boolean contains(T anEntry);
+
+    public T[] toArray();
+
+} // end BagInterface
