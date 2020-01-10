492ca57efd55bcf294cdf0d14d438728fabd9468
Stackingrule <messageswitching@gmail.com>
2020-01-10 22:58:27 +0800

    start

---
+++ JavaScript-DataStructures-and-Algo/test/string/0557.test.js
@@ -0,0 +1,5 @@
+import revertByWord from '../../codes/string/0557'
+
+test('revertByWord:Let\'s take LeetCode contest', () => {
+    expect(revertByWord("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+})
