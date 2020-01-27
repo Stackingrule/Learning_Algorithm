6e0e05ff1c92982cdd4029c5d7f4be016f4a19fa
Stackingrule <messageswitching@gmail.com>
2020-01-27 22:32:28 +0800

    add Algo-DataStructures-Java

---
+++ Algo-DataStructures-Java/src/BinaryOperation.java
@@ -0,0 +1,14 @@
+public class BinaryOperation {
+
+
+    public void swap_x(int x, int y) {
+        int tmp = y;
+        x = y;
+        y = tmp;
+    }
+
+    public static void main(String[] args) {
+
+        BinaryOperation BinaryOperation = new BinaryOperation();
+    }
+}
